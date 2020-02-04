b0b73fa5c40dc9be8a25d8d8454eb6031a7a40f5
arisa <arisankochira10@gmail.com>
2020-02-04 19:46:43 +0900

    infomationが変わらない

--- index.php
+++ index.php
@@ -4,43 +4,46 @@
 
 class Car
 {
-  public $name = "bob";
-  public $number = 21;
-  public $color = "red";
-
-  public function __construct($name, $number,$color)
+  private $name;
+  private $number;
+  private $color;
+  
+  public function __construct($name, $number, $color)
   {
     $this->name = $name;
     $this->number = $number;
     $this->color = $color;
   }
-
+  
   public function getName() {
     return $this->name;
   }
-
+  
   public function getNumber() {
     return $this->number;
   }
-
+  
   public function getColor() {
     return $this->color;
   }
-
+  
   public function setName($name) {
     $this->name = $name;
   }
-
+  
   public function setNumber($number) {
     $this->number = $number;
   }
-
+  
   public function setColor($color) {
     $this->color = $color;
   }
-
-  public function selfInfomation() {
-    echo "車の車種:" . $this->name . "、車体番号:" . $this->number . "、カラー:" . $this->color . "です。";
+  
+  public function setInfomation() {
+    echo $this->getInfomation();
+  }
+  public function getInfomation() {
+    return "車の車種:" . $this->name . "、車体番号:" . $this->number . "、カラー:" . $this->color . "です。";
   }
 }
 // 以下を実行した時にエラーがでないようにして下さい。
@@ -48,6 +51,7 @@ class Car
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
+// $car1->selfIntrodaction();
 // 車名(車種)を取得
 echo $car1->getName();
 echo '<br>';
@@ -61,11 +65,11 @@ echo $car1->getColor();
 echo '<br>';
 
 // 車の情報表示を表示
-echo $car1->selfInfomation();
+echo $car1->setInfomation();
 echo '<br>';
 
 // 車名(車種)を更新
-$car1->setName = 'アルファード';
+echo $car1->setName = 'アルファード';
 echo '<br>';
 
 // 車体番号を取得
@@ -76,6 +80,8 @@ echo '<br>';
 echo $car1->setColor = 'red';
 echo '<br>';
 
+
 // 車の情報表示を表示
-echo $car1->selfInfomation();
+// $car1->selfIntrodaction();
+echo $car1->setInfomation();
 echo '<br>';
